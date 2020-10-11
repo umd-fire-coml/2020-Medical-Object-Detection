874b695b9f150f30d323fa89dd06b203c0d3da05
KC10201 <kchen123@terpmail.umd.edu>
2020-10-11 19:31:18 -0400

    updated script to check for directories and correct files, import check_dir() and validate_dir()

--- data_dir_check.py
+++ data_dir_check.py
@@ -10,46 +10,83 @@ def check_dir ():
     home = os.path.expanduser('~')
     
     if not os.path.isdir('{}/data'.format(home)):
-        print('data directory does not exist')
+        print('[data] directory does not exist')
         return False
-    print('data directory exists')
+    print('[data] directory exists')
     perf = True
-    def validate_dir(name):
-        s = True
-        for filename in os.listdir(name):
-            if not re.match('normal_(\d{3}).tif', filename)and not re.match('tumor_(\d{3}).tif', filename):
-                print('{} is not part of validate directory.'.format(filename))
-                s = False
-        return True if s else False
+   
     #checks validate directory
     if not os.path.isdir('{}/data/validate'.format(home)):
-        print('validate directory does not exist')
+        print('[validate] directory does not exist')
         perf = False
     else:
         print('validate directory exists')
-        perf = validate_dir('{}/data/validate'.format(home))
+        if not os.path.isdir('{}/data/validate/tumor'.format(home)):
+            print('[tumor validate] directory does not exist')
+            perf = False
+        else: 
+            print('[tumor validate] directory exists')
+            x = validate_dir('{}/data/validate/tumor'.format(home), "tumor")
+            perf = x if perf else perf
+        if not os.path.isdir('{}/data/validate/normal'.format(home)):
+            print('[normal validate] directory does not exist')
+            perf = False
+        else:
+            print('[normal validate] directory exists')
+            x = validate_dir('{}/data/validate/normal'.format(home), "normal")
+            perf = x if perf else perf
     #checks test directory
     if not os.path.isdir('{}/data/test'.format(home)):
-        print('test directory does not exist')
+        print('[test] directory does not exist')
         perf = False
     else:
-        print('test directory exists')
-        x = validate_dir('{}/data/test'.format(home))
+        print('[test] directory exists')
+        x = validate_dir('{}/data/test'.format(home), "both")
         perf = x if perf else perf
     #checks train directory
     if not os.path.isdir('{}/data/train'.format(home)):
-        print('train directory does not exist')
+        print('[train] directory does not exist')
         perf = False
     else:
-        print('train directory exists')
-        x = validate_dir('{}/data/train'.format(home))
-        perf = x if perf else perf
-        
+        print('[train] directory exists')
+        if not os.path.isdir('{}/data/train/tumor'.format(home)):
+            print('[tumor train] directory does not exist')
+            perf = False
+        else: 
+            print('[tumor train] directory exists')
+            x = validate_dir('{}/data/train/tumor'.format(home), "tumor")
+            perf = x if perf else perf
+        if not os.path.isdir('{}/data/train/normal'.format(home)):
+            print('[normal train] directory does not exist')
+            perf = False
+        else:
+            print('[normal train] directory exists')
+            x = validate_dir('{}/data/train/normal'.format(home), "normal")
+            perf = x if perf else perf
+
     if not perf:
         return False
     print('Data directory and all subdirectories are correct, with correct data')
     return True
 
+def validate_dir(name, typ):
+    s = True
+
+    for filename in os.listdir(name):
+        if typ == "tumor":
+            if not re.match('tumor_(\d{3}).tif', filename):
+                print('[{}] is not part of [{}] directory.'.format(filename, name))
+                s = False
+        elif typ == "normal":
+            if not re.match('normal_(\d{3}).tif', filename):
+                print('[{}] is not part of [{}] directory.'.format(filename, name))
+                s = False
+        else:
+            if not re.match('normal_(\d{3}).tif', filename)and not re.match('tumor_(\d{3}).tif', filename):
+                print('[{}] is not part of [{}] directory.'.format(filename, name))
+                s = False
+        
+    return True if s else False
 
 
-
+check_dir()
