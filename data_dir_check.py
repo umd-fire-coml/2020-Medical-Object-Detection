ae4839e46a735a82c427c15edb14ff826473d71a
KC10201 <70864069+KC10201@users.noreply.github.com>
2020-11-10 20:24:25 -0500

    Update data_dir_check.py

--- data_dir_check.py
+++ data_dir_check.py
@@ -8,13 +8,13 @@ import re
 
 def check_dir ():
     home = os.path.expanduser('~')
-
+    
     if not os.path.isdir('{}/data'.format(home)):
         print('[data] directory does not exist')
         return False
     print('[data] directory exists')
     perf = True
-
+   
     #checks validate directory
     if not os.path.isdir('{}/data/validate'.format(home)):
         print('[validate] directory does not exist')
@@ -74,16 +74,18 @@ def validate_dir(name, typ):
 
     for filename in os.listdir(name):
         if typ == "tumor":
-            if not re.match('tumor_(\d{3}).tif', filename):
+            if not re.match('Copy of tumor_(\d{3}).tif', filename) and not re.match('tumor_(\d{3}).tif', filename):
                 print('[{}] is not part of [{}] directory.'.format(filename, name))
                 s = False
         elif typ == "normal":
-            if not re.match('normal_(\d{3}).tif', filename):
+            if not re.match('Copy of normal_(\d{3}).tif', filename) and not re.match('normal_(\d{3}).tif', filename):
                 print('[{}] is not part of [{}] directory.'.format(filename, name))
                 s = False
         else:
-            if not re.match('normal_(\d{3}).tif', filename)and not re.match('tumor_(\d{3}).tif', filename):
+            if not re.match('Copy of normal_(\d{3}).tif', filename)and not re.match('Copy of tumor_(\d{3}).tif', filename) and not re.match('normal_(\d{3}).tif', filename) and not re.match('tumor_(\d{3}).tif', filename):
                 print('[{}] is not part of [{}] directory.'.format(filename, name))
                 s = False
-
+        
     return True if s else False
+
+check_dir()
