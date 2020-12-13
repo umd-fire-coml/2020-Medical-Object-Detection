e0cff21fc40770042e788ded141f4e6e2a003d7d
KC10201 <70864069+KC10201@users.noreply.github.com>
2020-12-13 14:39:17 -0500

    Update data_dir_check.py
    changed data_dir_check.py to match with new dataset requirements

--- data_dir_check.py
+++ data_dir_check.py
@@ -1,67 +1,68 @@
 #train, validate, test
-#data
+#ISBI2016_ISIC_Part3_Training_Data
 #  | train
+#       | benign
+#       | maligmant
 #  | test
-#images titled: normal_00x.tif , tumor_00x.tif
+#       | benign
+#       | maligmant
+#images titled: ISIC_\d{7}.png
 import os
 import re
 
 def check_dir ():
     home = os.path.expanduser('~')
     
-    if not os.path.isdir('{}/data'.format(home)):
-        print('[data] directory does not exist')
+    if not os.path.isdir('{}/ISBI2016_ISIC_Part3_Training_Data'.format(home)):
+        print('[ISBI2016_ISIC_Part3_Training_Data] directory does not exist')
         return False
-    print('[data] directory exists')
+    print('[ISBI2016_ISIC_Part3_Training_Data] directory exists')
     perf = True
    
-    #checks validate directory
-    if not os.path.isdir('{}/data/validate'.format(home)):
-        print('[validate] directory does not exist')
-        perf = False
-    else:
-        print('validate directory exists')
-        if not os.path.isdir('{}/data/validate/tumor'.format(home)):
-            print('[tumor validate] directory does not exist')
-            perf = False
-        else: 
-            print('[tumor validate] directory exists')
-            x = validate_dir('{}/data/validate/tumor'.format(home), "tumor")
-            perf = x if perf else perf
-        if not os.path.isdir('{}/data/validate/normal'.format(home)):
-            print('[normal validate] directory does not exist')
-            perf = False
-        else:
-            print('[normal validate] directory exists')
-            x = validate_dir('{}/data/validate/normal'.format(home), "normal")
-            perf = x if perf else perf
     #checks test directory
-    if not os.path.isdir('{}/data/test'.format(home)):
+    if not os.path.isdir('{}/ISBI2016_ISIC_Part3_Training_Data/test'.format(home)):
         print('[test] directory does not exist')
         perf = False
     else:
         print('[test] directory exists')
-        x = validate_dir('{}/data/test'.format(home), "both")
-        perf = x if perf else perf
+        #checks if test/benign exists
+        if not os.path.isdir('{}/ISBI2016_ISIC_Part3_Training_Data/test/benign'.format(home)):
+            print('[benign test] directory does not exist')
+            perf = False
+        else: 
+            print('[benign test] directory exists')
+            x = validate_dir('{}/ISBI2016_ISIC_Part3_Training_Data/test/benign'.format(home))
+            perf = x if perf else perf
+        #checks if test/maligmant exists
+        if not os.path.isdir('{}/ISBI2016_ISIC_Part3_Training_Data/test/maligmant'.format(home)):
+            print('[maligmant test] directory does not exist')
+            perf = False
+        else:
+            print('[maligmant test] directory exists')
+            x = validate_dir('{}/ISBI2016_ISIC_Part3_Training_Data/test/maligmant'.format(home))
+            perf = x if perf else perf
+        
     #checks train directory
-    if not os.path.isdir('{}/data/train'.format(home)):
+    if not os.path.isdir('{}/ISBI2016_ISIC_Part3_Training_Data/train'.format(home)):
         print('[train] directory does not exist')
         perf = False
     else:
         print('[train] directory exists')
-        if not os.path.isdir('{}/data/train/tumor'.format(home)):
-            print('[tumor train] directory does not exist')
+        #checks if train/benign exists
+        if not os.path.isdir('{}/ISBI2016_ISIC_Part3_Training_Data/train/benign'.format(home)):
+            print('[benign train] directory does not exist')
             perf = False
         else: 
             print('[tumor train] directory exists')
-            x = validate_dir('{}/data/train/tumor'.format(home), "tumor")
+            x = validate_dir('{}/ISBI2016_ISIC_Part3_Training_Data/train/benign'.format(home))
             perf = x if perf else perf
-        if not os.path.isdir('{}/data/train/normal'.format(home)):
-            print('[normal train] directory does not exist')
+        #checks if train/maligmant exists
+        if not os.path.isdir('{}/ISBI2016_ISIC_Part3_Training_Data/train/maligmant'.format(home)):
+            print('[maligmant train] directory does not exist')
             perf = False
         else:
             print('[normal train] directory exists')
-            x = validate_dir('{}/data/train/normal'.format(home), "normal")
+            x = validate_dir('{}/ISBI2016_ISIC_Part3_Training_Data/train/maligmant'.format(home))
             perf = x if perf else perf
 
     if not perf:
@@ -69,25 +70,13 @@ def check_dir ():
     print('Data directory and all subdirectories are correct, with correct data')
     return True
 
-def validate_dir(name, typ):
+def validate_dir(name):
     s = True
-
     for filename in os.listdir(name):
-        if typ == "tumor":
-            if not re.match('Copy of tumor_(\d{3}).tif', filename) and not re.match('tumor_(\d{3}).tif', filename):
-                print('[{}] is not part of [{}] directory.'.format(filename, name))
-                s = False
-        elif typ == "normal":
-            if not re.match('Copy of normal_(\d{3}).tif', filename) and not re.match('normal_(\d{3}).tif', filename):
-                print('[{}] is not part of [{}] directory.'.format(filename, name))
-                s = False
-        else:
-            if not re.match('Copy of normal_(\d{3}).tif', filename)and not re.match('Copy of tumor_(\d{3}).tif', filename) and not re.match('normal_(\d{3}).tif', filename) and not re.match('tumor_(\d{3}).tif', filename):
-                print('[{}] is not part of [{}] directory.'.format(filename, name))
+        if not re.match('ISIC_(\d{7})\.png', filename):
+            print('[{}] is not part of [{}] directory.'.format(filename, name))
                 s = False
         
     return True if s else False
 
 check_dir()
-
-
